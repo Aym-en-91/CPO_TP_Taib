ab7b79071936daa7fe9115126affeda449ee4452
taibaymen <aymentaibat@gmail.com>
2023-09-25 13:01:51 +0200

    ajout exo 1

---
+++ exo1/src/exo1.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+* Taib Aymen
+* Grpupe : TD A
+* 25 septembre 2023
+ */
+/**
+ *
+ * @author aymen
+ */
+public class exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("au revoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour,voushabitez ou?");
+        prenom = sc.nextLine();
+    }
+
+}
