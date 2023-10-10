a73355e2b1f951621b855e70fa944387d55a5201
taibaymen <aymentaibat@gmail.com>
2023-10-11 00:58:41 +0200

    TP2 manip

---
+++ TP2_manip_Taib/src/tp2_manip_taib/Tartiflette.java
@@ -0,0 +1,19 @@
+package tp2_manip_taib;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aymen
+ */
+public class Tartiflette {
+     public int nbCalories;
+
+    public Tartiflette(int nbCalories) {
+        this.nbCalories = nbCalories;
+    }
+    
+}
