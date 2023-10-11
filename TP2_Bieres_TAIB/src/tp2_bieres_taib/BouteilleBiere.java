b81cfb91f8631c59290071789ab6172bf7ca368c
taibaymen <aymentaibat@gmail.com>
2023-10-11 10:55:08 +0200

    TP2 Bieres Taib

--- TP2_Bieres_TAIB/src/tp2_bieres_taib/BouteilleBiere.java
+++ TP2_Bieres_TAIB/src/tp2_bieres_taib/BouteilleBiere.java
@@ -48,15 +48,5 @@ public class BouteilleBiere {
     
     
 }
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author aymen
- */
-
-    
 
