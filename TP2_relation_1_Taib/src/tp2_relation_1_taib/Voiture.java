e97bb3bd76c2daa9db8987bf4fd68ec4b65b1017
taibaymen <aymentaibat@gmail.com>
2023-10-13 14:28:07 +0200

    TP2 relation Taib

---
+++ TP2_relation_1_Taib/src/tp2_relation_1_taib/Voiture.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_taib;
+
+/**
+ *
+ * @author aymen
+ */
+class Voiture {
+
+    Personne Proprietaire;
+
+    Voiture(String clio, String renault, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
