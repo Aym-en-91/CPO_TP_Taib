5fb271a998f8f0d4c7df266334b81c7f1e3006c1
taibaymen <aymentaibat@gmail.com>
2023-10-27 12:10:09 +0200

    LightOff_Taib_Versioj_Console

---
+++ LightOff_TAIB_version_console/src/lightoff_taib_version_console/CelluleLumineuse.java
@@ -0,0 +1,58 @@
+package lightoff_taib_version_console;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aymen
+ */
+public class CelluleLumineuse {
+    private boolean allumee;
+
+    /**
+     * Constructeur par défaut. Initialise la cellule comme éteinte.
+     */
+    public CelluleLumineuse() {
+        this.allumee = false;
+    }
+
+    /**
+     * Vérifie si la cellule est allumée.
+     *
+     * @return true si la cellule est allumée, sinon false.
+     */
+    public boolean isAllumee() {
+        return allumee;
+    }
+
+    /**
+     * Allume la cellule.
+     */
+    public void allumer() {
+        this.allumee = true;
+    }
+
+    /**
+     * Éteint la cellule.
+     */
+    public void eteindre() {
+        this.allumee = false;
+    }
+
+    /**
+     * Inverse l'état actuel de la cellule (allumée devient éteinte et vice versa).
+     */
+    public void inverserEtat() {
+        this.allumee = !this.allumee;
+    }
+
+    /**
+     * Réinitialise la cellule en l'éteignant.
+     */
+    public void reinitialiser() {
+        this.allumee = false;
+    }
+}
