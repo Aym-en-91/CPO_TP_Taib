b80683ccfb109130ea37be489fbcca11f3f1196d
taibaymen <aymentaibat@gmail.com>
2023-11-05 17:31:09 +0100

    LightOff_Taib_Versioj_Console

--- LightOff_Taib_version_console/src/lightoff_taib_version_console/LightOff_Taib_version_console.java
+++ LightOff_Taib_version_console/src/lightoff_taib_version_console/LightOff_Taib_version_console.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -14,8 +15,8 @@ public class LightOff_Taib_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Partie game = new Partie(5, 5);
-        game.initialiserPartie();
+        Partie game = new Partie();
+        game.choisirDifficulte();
 
         // Launch the game
         game.lancerPartie();
